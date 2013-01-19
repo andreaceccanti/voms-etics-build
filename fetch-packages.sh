31359906412e64428aa7141a17afa341f7492dd1
Andrea Ceccanti <andrea.ceccanti@cnaf.infn.it>
2013-01-19 09:17:42 +0100

    Move package fetching logic inside fetch-packages script.

---
+++ fetch-packages.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+base_url=https://raw.github.com/andreaceccanti/voms-etics-build
+
+if [ $# -lt 2 ]; then
+	echo "Wrong number of arguments!"
+	echo "Usage: $0 srcPackageDir packagesFile [tag]"
+	exit 1
+fi
+
+srcPackagesDir=$1
+packagesFile=$2
+tag=master
+
+if [ $# -eq 3]; then
+	tag=$3
+fi
+
+echo "Starting ETICS VOMS build..."
+echo "Downloading packages file..."
+wget --no-check-certificate $base_url/$tag/$packagesFile
+
+# Strip comments
+sed '/^#/d;/^$/d' $packagesFile > $packagesFile.stripped
+
+if [ ! -d "$srcPackagesDir" ]; then
+	echo "Building source packages dir $srcPackagesDir"
+	mkdir -P $srcPackagesDir
+fi
+
+echo "Downloading source packages..."
+wget --no-check-certificate -P $srcPackagesDir -i $packagesFile.stripped
+
+echo "Downloaded source packages:"
+ls -l  $srcPackagesDir
---
+++ metapackages.sl
@@ -0,0 +1,2 @@
+http://italiangrid.github.com/voms-mp/packages/1.1.0-1/emi-voms-mysql-1.1.0-1.src.rpm
+http://italiangrid.github.com/voms-mp/packages/1.1.0-1/emi-voms-oracle-1.1.0-1.src.rpm
